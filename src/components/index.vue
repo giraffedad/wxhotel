4c53a9caee35f749327f388d09db42bd7b16bce4
giraffedad <821745575@qq.com>
2017-12-27 02:07:41 +0800

    简单tab切换

---
+++ src/components/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="(item,index) in tabs" :class="{active:index == num}" @click="tab(index)">{{item}}</li>
+    </ul>
+    <div class="tabCon">
+      <div v-for='(itemCon,index) in tabContents' v-show=" index == num">{{itemCon}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      tabs: ["分时租赁", "整晚租赁", "整日租赁"],
+      tabContents: ["内容一", "内容二", "内容三"],
+      num: 1
+      }
+    },
+    methods:{
+      tab(index) {
+        this.num = index;
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  background-color: #adadad;
+}
+
+.hello ul {
+  display: flex;
+  justify-content:space-around;
+}
+
+.hello li{
+  width: 2.5rem;
+  height: 1rem;
+  text-align: center;
+}
+.tabCon{
+  width: 7.5rem;
+  height: 7.5rem;
+  background-color: #fffaaa;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import index from '@/components/index'
+import '../../libs/common.css'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      component: index
     }
   ]
 })
